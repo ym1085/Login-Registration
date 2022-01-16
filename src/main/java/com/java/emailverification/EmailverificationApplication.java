f93700004534ff22f5439ebdcd1781b77d96ee2d
ym1085 <youngmin1085@gmail.com>
2022-01-16 19:27:21 +0900

    CHORE: initalize commit
    1. 기본 셋팅 파일 업로드

---
+++ src/main/java/com/java/emailverification/EmailverificationApplication.java
@@ -0,0 +1,13 @@
+package com.java.emailverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailverificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailverificationApplication.class, args);
+	}
+
+}
